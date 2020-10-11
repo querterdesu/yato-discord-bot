3374b5d83e1a55f81079b185af1fc48ff18eac9d
Jan Cholewa <querter.ovutem@gmail.com>
2020-10-11 12:06:00 +0200

    Removed node_modules, help command and usage revamp

---
+++ commands/help.js
@@ -0,0 +1,45 @@
+const { prefix } = require ('../config.js')
+
+module.exports = {
+  name: 'help',
+  description: 'Get help on commands!',
+  aliases: ['cmds', 'commands', 'command', 'cmd'],
+  usage: '[command name]',
+  cooldown: 1,
+  execute(message, args) {
+    const data = [];
+    const { commands } = message.client();
+
+    if (!args.length) {
+      data.push('Here\'s a list of all of my commands:');
+      data.push(commands.map(command => command.name).join(', '));
+
+      return message.author.send(data, {split:true})
+        .then(() => {
+          if (message.channel.type === 'dm') return;
+          message.reply('I\'ve sent you a list of all of my commands!');
+        })
+        .catch(error => {
+          console.error(`Could not send commands to ${message.author.tag}.\n`, error);
+          message.reply('I could not DM you! Perhaps you have messages disabled?');
+        });
+    }
+
+    const name = args[0].toLowerCase();
+    const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+
+    if (!command) {
+      return message.reply('Could not find the command you were looking for!');
+    }
+
+    data.push(`**Name:** ${command.name}`);
+
+    if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
+    if (command.description) data.push(`**Description:** ${command.description}`);
+    if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+
+    data.push(`**Cooldown:** ${command.cooldown || 0} second(s)`)
+
+    message.channel.send(data, {split:true});
+  },
+}
