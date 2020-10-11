407e1a1c4f561c288de456bfb0b5bfa054db6461
Jan Cholewa <querter.ovutem@gmail.com>
2020-10-11 11:37:44 +0200

    update status

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ for (const file of commandFiles) {
 
 client.once('ready', () => {
 	console.log('Ready!');
-	client.user.setActivity('for any calls', {type: 'LISTENING'});
+	client.user.setActivity('any calls', {type: 'LISTENING'});
 });
 
 client.on('message', msg => {
