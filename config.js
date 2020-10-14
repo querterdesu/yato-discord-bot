4865660421ad34805fb6e3235fd4fb03fb95444e
Jan Cholewa <querter.ovutem@gmail.com>
2020-10-14 09:36:47 +0200

    Prefix change to //

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  'prefix': '!',
-  'token': 'NzAwMzI4NjEzOTI4NzYzNDgy.XphV2Q.mjpWC3RYS3PxDLWIy6pkBV-vYBk',
-}
+	'prefix': '//',
+	'token': 'NzAwMzI4NjEzOTI4NzYzNDgy.XphV2Q.mjpWC3RYS3PxDLWIy6pkBV-vYBk',
+};
