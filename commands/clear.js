c98839ea7840ed96195ca9c33638b7939fe7f417
Jan Cholewa <querter.ovutem@gmail.com>
2020-10-14 10:50:31 +0200

    new command clear

---
+++ commands/clear.js
@@ -0,0 +1,24 @@
+module.exports = {
+	name: 'clear',
+	description: 'Clear messages up to 100!',
+	aliases: ['clean', 'prune'],
+	args_required: 1,
+	max_args: 1,
+	usage: '<# of messages to clear>',
+	guildOnly: true,
+	cooldown: 5,
+	permissions: ['MANAGE_MESSAGES'],
+	execute(message, args) {
+		const amount = parseInt(args[0]) + 1;
+		if (isNaN(amount)) {
+			return message.channel.send('The argument you provided isn\'t a number.');
+		}
+		else if (amount < 1 || amount > 99) {
+			return message.channel.send('You input a wrong number, either too small or too large for the command.');
+		}
+		message.channel.bulkDelete(amount).catch(err => {
+			console.error(err);
+			message.channel.send('There was an error trying to clear these messages!');
+		});
+	},
+};
