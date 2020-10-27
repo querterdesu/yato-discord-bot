18557a982345429a99d7577bbb9f3fe09343fef2
Jan Cholewa <querter.ovutem@gmail.com>
2020-10-27 09:33:19 +0100

    reaction fix

--- commands/reaction.js
+++ commands/reaction.js
@@ -7,14 +7,14 @@ module.exports = {
 	max_args: 999,
 	cooldown: 0,
 	async execute(message, args) {
-		const sentMessage = message.channel.send('✅ | Reacting to this message')
-			.then(() => sentMessage.react('✅'))
-			.then(() => sentMessage.react('❌'));
+		const sMsg = message.channel.send('✅ | Reacting to this message')
+			.then((sentMessage) => sentMessage.react('✅'))
+			.then((sentMessage) => sentMessage.react('❌'));
 		const filter = (reaction, user) => {
 			return ['✅', '❌'].includes(reaction.emoji.name) && user.id === message.author.id;
 		};
 
-		message.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] }).then(collected => {
+		sMsg.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] }).then(collected => {
 			const reaction = collected.first();
 
 			if (reaction.emoji.name === '✅') {
