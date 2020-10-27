345547cdf0dbdf19aa32c23fdce269b7ec18f520
Jan Cholewa <querter.ovutem@gmail.com>
2020-10-27 11:11:16 +0100

    reaction fix

--- commands/reaction.js
+++ commands/reaction.js
@@ -7,23 +7,6 @@ module.exports = {
 	max_args: 999,
 	cooldown: 0,
 	execute(message, args) {
-		message.react('✅').then(() => { message.react('❌'); });
-
-		const filter = (reaction, user) => {
-			return ['✅', '❌'].includes(reaction.emoji.name) && user.id === message.author.id;
-		};
-
-		message.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] }).then(collected => {
-			const reaction = collected.first();
-
-			if (reaction.emoji.name === '✅') {
-				messageUtil.sendSuccess(message, 'You confirmed the message.');
-			}
-			else if (reaction.emoji.name === '❌') {
-				messageUtil.sendError(message, 'You denied the message.');
-			}
-
-		}).catch(collected => { messageUtil.sendError(message, 'You didn\'t react with anything.'); });
-
+		messageUtil.confirmPrompt(message, 60000);
 	},
 };
--- messages.js
+++ messages.js
@@ -8,4 +8,23 @@ module.exports = {
 	sendInfo(ref, message) {
 		return ref.channel.send(`❕ | ${message}`);
 	},
+	confirmPrompt(ref, waitingTime) {
+		ref.react('✅').then(() => { ref.react('❌'); });
+
+		const filter = (reaction, user) => {
+			return ['✅', '❌'].includes(reaction.emoji.name) && user.id === ref.author.id;
+		};
+
+		ref.awaitReactions(filter, { max: 1, time: waitingTime, errors: ['time'] }).then(collected => {
+			const reaction = collected.first();
+
+			if (reaction.emoji.name === '✅') {
+				return 1;
+			}
+			else if (reaction.emoji.name === '❌') {
+				return 0;
+			}
+
+		}).catch(collected => { console.error(`User did not react. ${collected}`); });
+	},
 };
