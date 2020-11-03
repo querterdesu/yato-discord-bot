d6ea2f54d5558b15efd09347159931752b996864
Jan Cholewa <querter.ovutem@gmail.com>
2020-11-03 08:43:59 +0100

    ban reaction

--- commands/reaction.js
+++ commands/reaction.js
@@ -7,15 +7,16 @@ module.exports = {
 	max_args: 999,
 	cooldown: 0,
 	execute(message, args) {
-		const confirmed = messageUtil.confirmPrompt(message, 60000);
-		if (confirmed === 1) {
-			messageUtil.sendSuccess(message, 'You accepted the prompt.');
-		}
-		else if (confirmed === 0) {
-			messageUtil.sendError(message, 'You denied the prompt.');
-		}
-		else {
-			messageUtil.sendError(message, 'You didn\'t react in time!');
-		}
+		const confirmed = messageUtil.confirmPrompt(message, 60000).then(() => {
+			if (confirmed === 1) {
+				messageUtil.sendSuccess(message, 'You accepted the prompt.');
+			}
+			else if (confirmed === 0) {
+				messageUtil.sendError(message, 'You denied the prompt.');
+			}
+			else {
+				messageUtil.sendError(message, 'You didn\'t react in time!');
+			}
+		});
 	},
 };
