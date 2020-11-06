f155a55dcb9f389a7fc85a1d27e9921d9b88fe00
Jan Cholewa <querter.ovutem@gmail.com>
2020-11-06 13:52:30 +0100

    spoofing fix

---
+++ commands/spoof.js
@@ -0,0 +1,16 @@
+const messageUtil = require('../messages.js');
+
+module.exports = {
+	name: 'spoof',
+	description: 'Spoof an event happening!',
+	aliases: ['simulate'],
+	usage: '[command name]',
+	args_required: 1,
+	max_args: 1,
+	cooldown: 3,
+	guildOnly: true,
+	permissions: ['MANAGE_SERVER'],
+	execute(message, args) {
+		message.client.emit(args[0], message.member);
+	},
+};
